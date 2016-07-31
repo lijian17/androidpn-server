e8c16d058a03dbddc0fda905ee14e8f978023833
lijian17 <374452668@qq.com>
2016-07-31 17:17:03 +0800

    控制器-处理与通知相关请求

--- androidpn-server/console/src/main/java/org/androidpn/server/console/controller/NotificationController.java
+++ androidpn-server/console/src/main/java/org/androidpn/server/console/controller/NotificationController.java
@@ -26,51 +26,52 @@ import org.springframework.web.bind.ServletRequestUtils;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.multiaction.MultiActionController;
 
-/** 
- * A controller class to process the notification related requests.  
- *
- * @author Sehwan Noh (devnoh@gmail.com)
+/**
+ * 控制器-处理与通知相关请求
+ * 
+ * @author lijian-pc
+ * @date 2016年7月31日 下午4:16:21
  */
 public class NotificationController extends MultiActionController {
 
-    private NotificationManager notificationManager;
+	private NotificationManager notificationManager;
 
-    public NotificationController() {
-        notificationManager = new NotificationManager();
-    }
+	public NotificationController() {
+		notificationManager = new NotificationManager();
+	}
 
-    public ModelAndView list(HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
-        ModelAndView mav = new ModelAndView();
-        // mav.addObject("list", null);
-        mav.setViewName("notification/form");
-        return mav;
-    }
+	public ModelAndView list(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		ModelAndView mav = new ModelAndView();
+		// mav.addObject("list", null);
+		mav.setViewName("notification/form");
+		return mav;
+	}
 
-    public ModelAndView send(HttpServletRequest request,
-            HttpServletResponse response) throws Exception {
-        String broadcast = ServletRequestUtils.getStringParameter(request,
-                "broadcast", "Y");
-        String username = ServletRequestUtils.getStringParameter(request,
-                "username");
-        String title = ServletRequestUtils.getStringParameter(request, "title");
-        String message = ServletRequestUtils.getStringParameter(request,
-                "message");
-        String uri = ServletRequestUtils.getStringParameter(request, "uri");
+	public ModelAndView send(HttpServletRequest request,
+			HttpServletResponse response) throws Exception {
+		String broadcast = ServletRequestUtils.getStringParameter(request,
+				"broadcast", "Y");
+		String username = ServletRequestUtils.getStringParameter(request,
+				"username");
+		String title = ServletRequestUtils.getStringParameter(request, "title");
+		String message = ServletRequestUtils.getStringParameter(request,
+				"message");
+		String uri = ServletRequestUtils.getStringParameter(request, "uri");
 
-        String apiKey = Config.getString("apiKey", "");
-        logger.debug("apiKey=" + apiKey);
+		String apiKey = Config.getString("apiKey", "");
+		logger.debug("apiKey=" + apiKey);
 
-        if (broadcast.equalsIgnoreCase("Y")) {
-            notificationManager.sendBroadcast(apiKey, title, message, uri);
-        } else {
-            notificationManager.sendNotifcationToUser(apiKey, username, title,
-                    message, uri);
-        }
+		if (broadcast.equalsIgnoreCase("Y")) {
+			notificationManager.sendBroadcast(apiKey, title, message, uri);
+		} else {
+			notificationManager.sendNotifcationToUser(apiKey, username, title,
+					message, uri);
+		}
 
-        ModelAndView mav = new ModelAndView();
-        mav.setViewName("redirect:notification.do");
-        return mav;
-    }
+		ModelAndView mav = new ModelAndView();
+		mav.setViewName("redirect:notification.do");
+		return mav;
+	}
 
 }
