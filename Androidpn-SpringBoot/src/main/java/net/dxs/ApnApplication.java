3cbf4850f4c91a1d6b5640da04cd1587aeec3917
深情小建 <374452668@qq.com>
2019-01-06 20:36:03 +0800

    修改入口类名称

--- Androidpn-SpringBoot/src/main/java/net/dxs/ApnApplication.java
+++ Androidpn-SpringBoot/src/main/java/net/dxs/ApnApplication.java
@@ -1,13 +1,13 @@
-package net.dxs.AndroidpnSpringBoot;
+package net.dxs;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AndroidpnSpringBootApplication {
+public class ApnApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AndroidpnSpringBootApplication.class, args);
+		SpringApplication.run(ApnApplication.class, args);
 	}
 
 }
