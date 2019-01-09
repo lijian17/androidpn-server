77ffa6c8b048ef78f419ab8615fdfac91c0403fe
深情小建 <374452668@qq.com>
2019-01-09 23:46:14 +0800

    工具类包加入到扫描

--- Androidpn-SpringBoot/src/main/java/net/dxs/ApnApplication.java
+++ Androidpn-SpringBoot/src/main/java/net/dxs/ApnApplication.java
@@ -10,7 +10,7 @@ import tk.mybatis.spring.annotation.MapperScan;
 //扫描mybatis mapper 包路径
 @MapperScan(basePackages = "net.dxs.mapper")
 //扫描 所有需要的包，包含一些自用的工具类包 所在的路径
-@ComponentScan(basePackages = { "net.dxs" })
+@ComponentScan(basePackages = { "net.dxs", "org.n3r.idworker" })
 public class ApnApplication {
 
 	public static void main(String[] args) {
